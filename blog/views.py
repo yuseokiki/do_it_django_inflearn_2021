f2cf329e6e8501959ec40663024b9e068e5df039
yuseokiki <yuseok0714@gmail.com>
2021-11-04 21:56:19 +0900

    pagination 추가하기:

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,8 @@ from .models import Post, Category, Tag
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
+
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
